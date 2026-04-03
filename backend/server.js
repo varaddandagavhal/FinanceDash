63e54cffe5e47305bb5726aa631cff903ec397b8
varad_dandagavha <77523268+varadbotdev@users.noreply.github.com>
2026-04-03 13:54:36 +0530

    Fix: Express 5 wildcard path in production

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use('/api/summary', require('./routes/summaryRoutes'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-  app.get('*', (req, res) =>
+  app.get('/*', (req, res) =>
     res.sendFile(path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html'))
   );
 } else {
